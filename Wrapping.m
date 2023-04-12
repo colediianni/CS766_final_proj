6db5b6bec7923052717c4fae1466f61fc43bb04a
ccoverte <129771787+ccoverte@users.noreply.github.com>
2023-04-12 12:48:16 -0500

    Update Wrapping.m

--- Wrapping.m
+++ Wrapping.m
@@ -13,8 +13,8 @@ end
 figure(1)
 montage(image)
 
-x = 5; 
-image{x}=flip(image{x},2);
+x = 3; 
+image{x}=flip(image{x},1);
 
 figure(2)
 montage(image)
@@ -33,14 +33,13 @@ montage(image)
 %polscope = imbinarize(image{5},0.1);
 polscope = rgb2gray(image{2});
 %BF = 1-imbinarize(rgb2gray(image{1}),0.5);
-BF = rgb2gray(image{1});
+BF = rgb2gray(image{3});
 montage({polscope, BF})
 %%
 
-Pol_2 = imresize(polscope, 0.5);
-BF_2 = imresize(BF, 0.25);
-% figure()
-% montage({Pol_2, TRI_2})
+Pol_2 = imresize(polscope, 0.25);
+BF_2 = imresize(BF, 0.5);
+
 %%
 [xs, xd] = genSIFTMatches(im2single(Pol_2), im2single(BF_2));
 
@@ -60,8 +59,8 @@ BF_3 = padarray(BF_2, (size(Pol_2, 1, 2)-size(BF_2, 1, 2)), 0, 'post');
 showCorrespondence( im2single(Pol_2), im2single(BF_3), xs, xd);
 %%
 
-ransac_n = 3000; % Max number of iterations
-ransac_eps = 100; %Acceptable alignment error 
+ransac_n = 100000; % Max number of iterations
+ransac_eps = 50; %Acceptable alignment error 
 
 [inliers_id, H_3x3] = runRANSAC(xs, xd, ransac_n, ransac_eps);
 
@@ -88,35 +87,11 @@ showCorrespondence(im2single(Pol_2), im2single(BF_3), xs(inliers_id,:), xd(inlie
 % hold on
 % plot(xd(inliers_id, 1), xd(inliers_id, 2), 'ro', 'MarkerFaceColor','r')
 
-%% select points
 
-% figure(3)
-% imshow(polscope)
-% [x_ps, y_ps] = ginput(4);
-% hold on
-% scatter(x_ps, y_ps, 'filled')
-% figure(4)
-% imshow(HE)
-% [x_HE, y_HE] = ginput(4);
-% figure(5)
-% imshow(TRI)
-% [x_TRI, y_TRI] = ginput(4);
-% figure(6)
-% imshow(PSR_BF)
-% [x_BF, y_BF] = ginput(4);
-% figure(7)
-% imshow(PSR_POL)
-% [x_pol, y_pol] = ginput(4);
+%% apply homology
 
-%% compute homology
+ 
+[mask, result_img]=backwardWarpImg(Pol_2, inv(H_3x3), size(BF_2,2,1));
 
-% scr_pts = cat(3, [x_HE, y_HE], [x_TRI, y_TRI], [x_BF, y_BF]);
-% 
-% for n = 1:3
-%     H(:,:,n) = computeHomography([x_ps, y_ps],scr_pts(:,:,n));
-% end
-% 
-% [mask, result_img]=backwardWarpImg(polscope, inv(H(:,:,2)), size(TRI,2,1));
-% 
-% figure(8)
-% imshow(uint16(result_img(:,:,1)))
+figure()
+imshow(result_img)
