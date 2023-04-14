0efeb769a8db53256cc8ea7301f62f69d43f0e4f
ccoverte <129771787+ccoverte@users.noreply.github.com>
2023-04-14 16:14:18 -0500

    Update Wrapping.m

--- Wrapping.m
+++ Wrapping.m
@@ -13,8 +13,8 @@ end
 figure(1)
 montage(image)
 
-x = 3; 
-image{x}=flip(image{x},1);
+x = 6; 
+image{x}=flip(image{x},2);
 
 figure(2)
 montage(image)
@@ -30,9 +30,9 @@ montage(image)
 
 %% run sift
 
-%polscope = imbinarize(image{5},0.1);
+%polscope = imbinarize(image{6},0.1);
 polscope = rgb2gray(image{2});
-%BF = 1-imbinarize(rgb2gray(image{1}),0.5);
+%BF = 1-imbinarize(rgb2gray(image{2}),0.6);
 BF = rgb2gray(image{3});
 montage({polscope, BF})
 %%
@@ -43,39 +43,39 @@ BF_2 = imresize(BF, 0.5);
 %%
 [xs, xd] = genSIFTMatches(im2single(Pol_2), im2single(BF_2));
 
-figure()
-imshow(Pol_2)
-hold on
-plot(xs(:,1), xs(:,2), 'ro', 'MarkerFaceColor','r')
-
-figure()
-imshow(BF_2)
-hold on
-plot(xd(:,1), xd(:,2), 'ro', 'MarkerFaceColor','r')
-
-BF_3 = padarray(BF_2, (size(Pol_2, 1, 2)-size(BF_2, 1, 2)), 0, 'post');
-%Pol_3 = padarray(Pol_2, (size(BF_2, 1, 2)-size(Pol_2, 1, 2)), 0, 'post');
 % figure()
-showCorrespondence( im2single(Pol_2), im2single(BF_3), xs, xd);
-%%
+% imshow(Pol_2)
+% hold on
+% plot(xs(:,1), xs(:,2), 'ro', 'MarkerFaceColor','r')
+% 
+% figure()
+% imshow(BF_2)
+% hold on
+% plot(xd(:,1), xd(:,2), 'ro', 'MarkerFaceColor','r')
+
+%BF_3 = padarray(BF_2, (size(Pol_2, 1, 2)-size(BF_2, 1, 2)), 0, 'post');
+Pol_3 = padarray(Pol_2, (size(BF_2, 1, 2)-size(Pol_2, 1, 2)), 0, 'post');
+% figure()
+showCorrespondence( im2single(Pol_3), im2single(BF_2), xs, xd);
+%
 
 ransac_n = 100000; % Max number of iterations
-ransac_eps = 50; %Acceptable alignment error 
+ransac_eps = 10; %Acceptable alignment error 
 
 [inliers_id, H_3x3] = runRANSAC(xs, xd, ransac_n, ransac_eps);
 
-figure()
-imshow(Pol_2)
-hold on
-plot(xs(inliers_id,1), xs(inliers_id,2), 'ro', 'MarkerFaceColor','r')
-
-figure()
-imshow(BF_2)
-hold on
-plot(xd(inliers_id,1), xd(inliers_id,2), 'ro', 'MarkerFaceColor','r')
-
-figure()
-showCorrespondence(im2single(Pol_2), im2single(BF_3), xs(inliers_id,:), xd(inliers_id, :));
+% figure()
+% imshow(Pol_2)
+% hold on
+% plot(xs(inliers_id,1), xs(inliers_id,2), 'ro', 'MarkerFaceColor','r')
+% 
+% figure()
+% imshow(BF_2)
+% hold on
+% plot(xd(inliers_id,1), xd(inliers_id,2), 'ro', 'MarkerFaceColor','r')
+% 
+% figure()
+showCorrespondence(im2single(Pol_3), im2single(BF_2), xs(inliers_id,:), xd(inliers_id, :));
 % 
 % figure()
 % imshow(HE)
