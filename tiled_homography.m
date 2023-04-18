ba61f257a259de8285ffa364677b54f2f1c5b53f
colediianni <cwdiianni@gmail.com>
2023-04-18 16:49:11 -0500

    Update tiled_homography.m

--- tiled_homography.m
+++ tiled_homography.m
@@ -1,45 +1,136 @@
 function stitched_img = tiled_homography(img1, img2, num_y_patches, num_x_patches)
 
-assert(isequal(size(img1), size(img2)))
-patch_size_y = size(img1, 1)/num_y_patches;
-patch_size_x = size(img1, 2)/num_x_patches;
-assert(floor(patch_size_y)==patch_size_y)
-assert(floor(patch_size_x)==patch_size_x)
-
-disp(size(img1))
-
-img1_patches = mat2cell(img1,zeros(num_y_patches,1) + floor(patch_size_y), zeros(num_x_patches,1) + floor(patch_size_x), 3);
-% disp(img1_patches)
-img2_patches = mat2cell(img2,zeros(num_y_patches,1) + floor(patch_size_y), zeros(num_x_patches,1) + floor(patch_size_x), 3);
+% img1 = imread(fullfile('images', 'Day 3', 'NMI-D3-92222-001-20x.ome.tif'));
+img1 = imread(fullfile('images', 'Day 5', 'NMID5_Tri_02_10x.ome.tif (RGB).tif'));
+img1 = imresize(img1,0.1);
+% img2 = imread(fullfile('images', 'Day 3', 'stitched_Day3_HE_slide1.tif'));
+img2 = imread(fullfile('images', 'Day 5', 'NMI-D5-92222-001-20x.ome.tif'));
+% img2 = flip(img2, 2);
+img2 = imresize(img2,0.1);
+img1 = imbinarize(rgb2gray(img1), "adaptive");
+img2 = imbinarize(rgb2gray(img2), "adaptive");
 
 
 impl = 'MATLAB';
+[xs, xd] = genSIFTMatches(img1, img2, impl);
+
+figure()
+imshow(img1)
+hold on
+plot(xs(:,1), xs(:,2), 'ro', 'MarkerFaceColor','r')
+ 
+figure()
+imshow(img2)
+hold on
+plot(xd(:,1), xd(:,2), 'ro', 'MarkerFaceColor','r')
+
+
+disp(size(xs))
+ransac_n = 3000;
+ransac_eps = 100;
+[inliers_id, H_3x3] = runRANSAC(xs, xd, ransac_n, ransac_eps);
+disp(size(inliers_id))
+
+stop
+
+% figure()
+% imshow(img1)
+% pause(2)
+% figure()
+% imshow(img2)
+% pause(2)
+
+% assert(isequal(size(img1), size(img2)))
+
+if floor(size(img1, 1) / 2) ~= size(img1, 1) / 2
+    img1 = img1(1:size(img1, 1)-1, :);
+end
+if floor(size(img1, 2) / 2) ~= size(img1, 2) / 2
+    img1 = img1(:, 1:size(img1, 2)-1);
+end
+patch_size_y1 = size(img1, 1)/num_y_patches;
+patch_size_x1 = size(img1, 2)/num_x_patches;
+assert(floor(patch_size_y1)==patch_size_y1)
+assert(floor(patch_size_x1)==patch_size_x1)
+
+if floor(size(img2, 1) / 2) ~= size(img2, 1) / 2
+    img2 = img2(1:size(img2, 1)-1, :);
+end
+if floor(size(img2, 2) / 2) ~= size(img2, 2) / 2
+    img2 = img2(:, 1:size(img2, 2)-1);
+end
+patch_size_y2 = size(img2, 1)/num_y_patches;
+patch_size_x2 = size(img2, 2)/num_x_patches;
+assert(floor(patch_size_y2)==patch_size_y2)
+assert(floor(patch_size_x2)==patch_size_x2)
+% disp(size(img1, 1))
+% disp(size(img1, 2))
+
+
+% TODO: cut patch from one image and resize it to be the same as other
+% image + 2*patch_size
+
+
+% img1_patches = mat2cell(img1,zeros(num_y_patches,1) + floor(patch_size_y1), zeros(num_x_patches,1) + floor(patch_size_x1));
+% disp(img1_patches)
+% img2_patches = mat2cell(img2,zeros(num_y_patches,1) + floor(patch_size_y2), zeros(num_x_patches,1) + floor(patch_size_x2));
+
+% stop
+
+padding = 100;
+
+impl = 'MATLAB';
 ransac_n = 100; % Max number of iterations
-ransac_eps = 2; % Acceptable alignment error 
+ransac_eps = 40; % Acceptable alignment error 
 
 for y_patch_idx = 1:num_y_patches
     for x_patch_idx = 1:num_x_patches
 
-        patch1 = img1_patches{y_patch_idx, x_patch_idx};
-        patch2 = img2_patches(max(1, y_patch_idx-1):min(num_y_patches, y_patch_idx+1), max(1, x_patch_idx-1):min(num_x_patches, x_patch_idx+1));
-        patch2 = cell2mat(patch2);
-        imshow(patch2)
-        pause(0.5)
+%         patch1 = img1_patches{y_patch_idx, x_patch_idx};
+%         patch2 = img2_patches(min(1, y_patch_idx-1):min(num_y_patches, y_patch_idx+1), min(1, x_patch_idx-1):min(num_x_patches, x_patch_idx+1));
+        patch1 = img1(max(1, 1+(y_patch_idx-1)*patch_size_y1 - padding):min(size(img1, 1), y_patch_idx*patch_size_y1 + padding), max(1, 1+(x_patch_idx-1)*patch_size_x1 - padding):min(size(img1, 2), x_patch_idx*patch_size_x1 + padding));
+%         patch2 = cell2mat(patch2);
+%         disp(size(patch1))
+%         imshow(patch1)
+%         pause(0.5)
 
+        patch2 = img2(1+(y_patch_idx-1)*patch_size_y2:y_patch_idx*patch_size_y2, 1+(x_patch_idx-1)*patch_size_x2:x_patch_idx*patch_size_x2);
+%         disp(size(patch2))
 
         [xs, xd] = genSIFTMatches(patch1, patch2, impl);
-        disp(size(xs))
-        disp(size(xd))
-        if size(xs, 1) >= 4
-            [inliers_id, H_3x3] = runRANSAC(xs, xd, ransac_n, ransac_eps);
-            dest_canvas_width_height = [size(patch2, 2), size(patch2, 1)];
-            [dest_img_mask, dest_img] = backwardWarpImg(patch1, inv(H_3x3), dest_canvas_width_height);
+
+        figure()
+        imshow(patch1)
+        hold on
+        plot(xs(:,1), xs(:,2), 'ro', 'MarkerFaceColor','r')
         
-            stitched_img_mask = zeros(dest_canvas_width_height(2), dest_canvas_width_height(1));
-            disp(size(stitched_img_mask))
-            disp(size(dest_img_mask))
-            blended_result = blendImagePair(patch2, stitched_img_mask, dest_img, dest_img_mask, 'overlay');
-%             stitched_img = blended_result;
-        end
+        figure()
+        imshow(patch2)
+        hold on
+        plot(xd(:,1), xd(:,2), 'ro', 'MarkerFaceColor','r')
+
+%         disp(size(xs))
+%         disp(size(xd))
+%         ransac_n = 100 * size(xs, 1);
+%         if size(xs, 1) >= 4
+%             [inliers_id, H_3x3] = runRANSAC(xs, xd, ransac_n, ransac_eps);
+%             disp(size(inliers_id))
+%             dest_canvas_width_height = [size(patch1, 2), size(patch1, 1)];
+%             [dest_img_mask, dest_img] = backwardWarpImg(patch2, inv(H_3x3), dest_canvas_width_height);
+% %             imshow(dest_img_mask)
+% %             pause(1)
+%             imshow(dest_img)
+%             pause(1)
+% %         
+% %             stitched_img_mask = zeros(dest_canvas_width_height(2), dest_canvas_width_height(1));
+% %             disp(size(stitched_img_mask))
+% %             disp(size(dest_img_mask))
+% %             blended_result = blendImagePair(patch2, stitched_img_mask, dest_img, dest_img_mask, 'overlay');
+% % %             stitched_img = blended_result;
+%         else
+%             disp("not enough ransac points to compute homography")
+%             % TODO put image1 into the center patch of image 2
+%             % TODO consider averageing the homographies of surrounding patches
+%         end
     end
 end
