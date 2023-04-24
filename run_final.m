1fcc12d473eb153de7e67e05dfbd105a1ca8e77d
hwilson23 <111527077+hwilson23@users.noreply.github.com>
2023-04-24 17:20:57 -0500

    Update run_final.m

--- run_final.m
+++ run_final.m
@@ -1,12 +1,12 @@
 function run_final()
 
-% img1 = imread(fullfile('images', 'Day 3', 'NMI-D3-92222-001-20x.ome.tif'));
+ img1 = imread(fullfile('images', 'Day 3', 'NMI-D3-92222-001-20x.ome.tif'));
 % img1 = imread(fullfile('images', 'Day 5', 'NMID5_Tri_02_10x.ome.tif (RGB).tif'));
-img1 = imread(fullfile('images', 'Day 1 R', 'MIR_D1_HE.tif'));
+%img1 = imread(fullfile('images', 'Day 1 R', 'MIR_D1_HE.tif'));
 img1 = imresize(img1,0.08);
-% img2 = imread(fullfile('images', 'Day 3', 'stitched_Day3_HE_slide1.tif'));
+ img2 = imread(fullfile('images', 'Day 3', 'stitched_Day3_HE_slide1.tif'));
 % img2 = imread(fullfile('images', 'Day 5', 'NMI-D5-92222-001-20x.ome.tif'));
-img2 = imread(fullfile('images', 'Day 1 R', 'NMIRD1-11323-002-10x.ome.tif'));
+%img2 = imread(fullfile('images', 'Day 1 R', 'NMIRD1-11323-002-10x.ome.tif'));
 img2 = flip(img2, 1);
 img2 = imresize(img2,0.1);
 img1 = imbinarize(img1, 0.09);
@@ -19,8 +19,7 @@ end
 if floor(size(img1, 2) / 2) ~= size(img1, 2) / 2
     img1 = img1(:, 1:size(img1, 2)-1);
 end
-assert(floor(patch_size_y1)==patch_size_y1)
-assert(floor(patch_size_x1)==patch_size_x1)
+
 
 if floor(size(img2, 1) / 2) ~= size(img2, 1) / 2
     img2 = img2(1:size(img2, 1)-1, :);
@@ -28,8 +27,7 @@ end
 if floor(size(img2, 2) / 2) ~= size(img2, 2) / 2
     img2 = img2(:, 1:size(img2, 2)-1);
 end
-assert(floor(patch_size_y2)==patch_size_y2)
-assert(floor(patch_size_x2)==patch_size_x2)
+
 
 
 
@@ -38,7 +36,7 @@ unique(stitched_img)
 
 %sliceoutput({stitched_img, img1})
 figure();imshowpair(stitched_img,img1)
-figure(); imshow(img1-stitched_img)
+figure(); imshow(abs(img1-stitched_img))
 disp('value')
 [row,col] = size(img1);
 sum(abs(img1-stitched_img),'all')/(row*col)
@@ -55,7 +53,7 @@ unique(dest_img)
 
 %sliceoutput({stitched_img, img1})
 figure();imshowpair(dest_img,img1)
-figure(); imshow(img1-dest_img)
+figure(); imshow(abs(img1-dest_img))
 disp('value')
 [row,col] = size(img1);
 sum(abs(img1-dest_img),'all')/(row*col)
