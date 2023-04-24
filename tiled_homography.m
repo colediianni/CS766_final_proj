59bb6cd9c67bc44ee5fec2325712e1837bdbd5fd
colediianni <cwdiianni@gmail.com>
2023-04-24 16:25:18 -0500

    added run_final

--- tiled_homography.m
+++ tiled_homography.m
@@ -1,19 +1,5 @@
 function [stitched_img, stitched_img_mask] = tiled_homography(img1, img2, num_y_patches, num_x_patches)
 
-% img1 = imread(fullfile('images', 'Day 3', 'NMI-D3-92222-001-20x.ome.tif'));
-% img1 = imread(fullfile('images', 'Day 5', 'NMID5_Tri_02_10x.ome.tif (RGB).tif'));
-img1 = imread(fullfile('images', 'Day 1 R', 'MIR_D1_HE.tif'));
-img1 = imresize(img1,0.08);
-% img2 = imread(fullfile('images', 'Day 3', 'stitched_Day3_HE_slide1.tif'));
-% img2 = imread(fullfile('images', 'Day 5', 'NMI-D5-92222-001-20x.ome.tif'));
-img2 = imread(fullfile('images', 'Day 1 R', 'NMIRD1-11323-002-10x.ome.tif'));
-img2 = flip(img2, 1);
-img2 = imresize(img2,0.1);
-img1 = imbinarize(img1, 0.09);
-img2 = 1 - imbinarize(rgb2gray(img2), 0.45);
-img2 = img2(250:850, 400:1250);
-
-
 % impl = 'MATLAB';
 % [xs, xd] = genSIFTMatches(img1, img2, impl);
 % 
